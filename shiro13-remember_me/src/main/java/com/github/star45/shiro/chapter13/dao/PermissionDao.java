49a18f4a4a75195d5dffc39051528f388a8a65bc
仰望星空 <starsxlk@gmail.com>
2015-09-16 13:12:33 +0800

    增加第十三章 rememberMe
    Signed-off-by: 仰望星空 <starsxlk@gmail.com>

---
+++ shiro13-remember_me/src/main/java/com/github/star45/shiro/chapter13/dao/PermissionDao.java
@@ -0,0 +1,28 @@
+/** 
+* @file     PermissionDao.java 
+* @brief    shiro06-realm's file 
+* @author   许立亢 
+* @date     2015年9月7日
+* @par Copyright (c) 2015 , xulikang@sbr-info.com All Rights Reserved
+*/ 
+
+package com.github.star45.shiro.chapter13.dao;
+
+import com.github.star45.shiro.chapter13.entity.Permission;
+
+/**
+ * @brief 类简短说明
+ * @details 详细说明 
+ * @warning 注意事项
+ * @date 2015年9月7日
+ * @author 许立亢 
+ * @version 1.0
+ * @ingroup g_scmcc_power_model
+ */
+
+public interface PermissionDao {
+	public Permission createPermission(Permission permission);
+
+    public void deletePermission(Long permissionId);
+
+}
---
+++ shiro13-remember_me/src/main/java/com/github/star45/shiro/chapter13/entity/Role.java
@@ -0,0 +1,97 @@
+/** 
+* @file     Role.java 
+* @brief    shiro06-realm's file 
+* @author   许立亢 
+* @date     2015年9月7日
+* @par Copyright (c) 2015 , xulikang@sbr-info.com All Rights Reserved
+*/ 
+
+package com.github.star45.shiro.chapter13.entity;
+
+import java.io.Serializable;
+
+/**
+ * @brief 类简短说明
+ * @details 详细说明 
+ * @warning 注意事项
+ * @date 2015年9月7日
+ * @author 许立亢 
+ * @version 1.0
+ * @ingroup g_scmcc_power_model
+ */
+
+@SuppressWarnings("serial")
+public class Role implements Serializable{
+	private Long id;
+    private String role; //角色标识 程序中判断使用,如"admin"
+    private String description; //角色描述,UI界面显示使用
+    private Boolean available = Boolean.FALSE; //是否可用,如果不可用将不会添加给用户
+
+    public Role() {
+    }
+
+    public Role(String role, String description, Boolean available) {
+        this.role = role;
+        this.description = description;
+        this.available = available;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                ", description='" + description + '\'' +
+                ", available=" + available +
+                '}';
+    }
+}
